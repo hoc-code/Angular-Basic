df564013a0cdcb90f8fb4e45a9755ba0dd5a2519
Long <binhnhi210203@gmail.com>
2024-11-11 22:07:29 +0700

    buổi học ngày 11/11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderLayoutComponent } from './shared/header-layout/header.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderLayoutComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-basic-project';
+  // text
+  title = {
+    name: 'Long Pé',
+    old: 21,
+
+  };
+
+  //properties
+  isDisable= true;
+
+  // attributes
+  contentImage = 'LongPe';
+  
+  bindingMessage = '';
+
+  nameBtn = 'Click me!';
+  clickMessage = '';
+
+  handleClickMe(): void{
+    this.clickMessage = 'Xin chào tôi là Long Pé.';
+  }
+
+  updateField(): void{
+    console.log('Hello');
+  }
+
 }
